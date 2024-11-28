9cfdfdbcfbb6e5a06c0e5cfa8d4a701698775b2c
shanacruzat <shanaczanecruzat@gmail.com>
2024-11-28 12:47:31 +0800

    eawofh

--- finals.cpp
+++ finals.cpp
@@ -105,13 +105,106 @@ class ParkInnLodge {
 
 //class Employee
 class Employee{
-  public:
+  private:
+  vector<User> users;
 
+  public:
   Employee() {}
 
   void manageUserAccount(){
+    int choice;
 
+    do {
+      cout << "\n--- User Account Management ---\n";
+      cout << "1. Register New User\n";
+      cout << "2. View User Account Information\n";
+      cout << "3. Update User Details\n";
+      cout << "4. Delete User Account\n";
+      cout << "5. Exit\n";
+      cout << "Enter your choice: ";
+      cin >> choice;
+      
+      switch(choice){
+        case 1:
+          registerUser();
+          break;
+        case 2:
+          viewUser();
+          break;
+        case 3:
+          updateUser();
+          break;
+        case 4: 
+          deleteUser();
+          break;
+        case 5:
+          cout << "Exiting User Account Management...\n";
+          break;
+        default:
+          cout << "Invalid choice. Please try again.\n";
+      }
+    } while (choice != 5);
   }
+
+  private:
+
+  void registerUser(){
+    User newUser;
+    cout << "Enter Name: ";
+    cin.ignore();
+    getline(cin, newUser.name);
+    cout << "Enter Email: ";
+    cin >> newUser.email;
+    cout << "Enter Password: ";
+    cin >> newUser.password;
+    users.push_back(newUser);
+    cout << "User registered successfully!\n";
+  }
+
+  void viewUser(){
+    string email;
+    cout<< "Enter email of the user to view: ";
+    cin >> email;
+  }
+
+  bool found = false;
+  for (const auto& user : users){
+    if (user.email == email){
+      cout << "\n--- User Details ---\n";
+      cout << "Name: " << user.name << "\n";
+      cout << "Email: " << user.email << "\n";
+      found = truebreak;
+    }
+  }
+
+  if(!found){
+    cout << "User not found.\n";
+  }
+}
+
+void updateUser(){
+  string email;
+  cout << "Enter email of the user to update: ";
+  cin >> email;
+
+  bool found = false;
+  for (auto&user : users){
+    if (user.email == email){
+      cout << "Enter new name: ";
+      cin.ignore;
+      getline(cin, user.name);
+      cout << "Enter new password: ";
+      cin >> user.password;
+      cout << "User details updated successfully!\n";
+      found = true;
+      break;
+    }
+  }
+
+  if(!found){
+    cout << "User not found.\n";
+  }
+}
 
   void addRoom() {
 
